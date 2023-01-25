09cfeda9c4470b8a25dcc3f522937372f544ae21
abhishek <abhishekkumarssm92@gmail.com>
2023-01-25 10:30:47 +0530

    class abhishek add

---
+++ src/com/velocity/git/Abhishek.java
@@ -0,0 +1,9 @@
+package com.velocity.git;
+
+public class Abhishek {
+	public static void main(String[] args) {
+		
+		System.out.println("sucessfully");
+	}
+
+}
--- src/com/velocity/git/Test1.java
+++ src/com/velocity/git/Test1.java
@@ -4,9 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("project 2 exported sucessfully");
 		
 		System.out.println("Macking change");
+		System.out.println("project  sucessfully");
 
 	}
 
